aee71053cb653b0f712aca2b6e0df72b13f088e6
Shreeya Mody <shreeyamody07@gmail.com>
2026-03-29 19:33:55 -0500

    Implement card click function
    Add click event handler to limit card selections

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+box.onclick = function () {
+    // Prevent clicking more than 2 cards or clicking the same card twice
+    if (document.querySelectorAll('.boxOpen').length < 2) {
+        this.classList.add('boxOpen');
+    }
